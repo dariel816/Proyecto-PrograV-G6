34e32ba1caafa5f27cfc4fb8575fa43323c9da92
Jose Guerrero <guerrro161289@gmail.com>
2026-05-28 21:07:06 -0600

    Add project files.

---
+++ Forms/FrmProductos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SistemaVentas.Datos.DAO;//Importación del espacio de nombres que contiene la clase ProductoDAO para acceder a los métodos de datos relacionados con productos
+
+
+
+namespace SistemadeVentas.Presentacion.Forms
+
+
+{
+    public partial class FrmProductos : Form
+    {
+        ProductoDAO productoDAO = new ProductoDAO();//Instancia de ProductoDAO para acceder a los métodos de datos relacionados con productos
+
+        public FrmProductos()
+        {
+            InitializeComponent();
+            CargarProductos();//Llamada al método para cargar los productos en el DataGridView al iniciar el formulario
+        }
+
+        private void CargarProductos()
+        {
+            dgvProductos.DataSource = productoDAO.ObtenerProductos();
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
